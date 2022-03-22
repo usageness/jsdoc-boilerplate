df59e5f8baf190d54bb2234ceb80f4e3b3140b23
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-03-22 12:29:22 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 class VendingMachine {
     /**
      * vendingMachine 클래스 생성
-		 * 따뜻한 아메리카노, 아이스 아메리카노는 기본값으로 생성되어 있다.
+     * 따뜻한 아메리카노, 아이스 아메리카노는 기본값으로 생성되어 있다.
      */
     constructor() {
         // ...
-		}
+    }
 
     /**
      * 투입 금액에 따라 커피를 구입하는 메서드.
@@ -17,7 +17,7 @@ class VendingMachine {
 
     /**
      * 커피를 구입했을때 콘솔 메시지를 출력하는 메서드.
-		 * 인자로 item 객체를 받습니다.
+     * 인자로 item 객체를 받습니다.
      */
     displayPurchaseMessage(item) {
         // ...
